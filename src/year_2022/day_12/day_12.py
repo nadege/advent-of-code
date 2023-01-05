a501925d1ecfd4e9902a7e2c4216de8aa7b9ab0f
Nadege Michel <michel.nadege@gmail.com>
2023-01-05 10:37:24 +0000

    day 12

---
+++ src/year_2022/day_12/day_12.py
@@ -0,0 +1,173 @@
+import math
+from typing import List
+
+
+# Credit for this: Nicholas Swift
+# as found at https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+from warnings import warn
+import heapq
+
+class Node:
+    """
+    A node class for A* Pathfinding
+    """
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+    
+    def __repr__(self):
+        return f"{self.position} - g: {self.g} h: {self.h} f: {self.f}"
+
+    # defining less than for purposes of heap queue
+    def __lt__(self, other):
+        return self.f < other.f
+    
+    # defining greater than for purposes of heap queue
+    def __gt__(self, other):
+        return self.f > other.f
+
+def return_path(current_node):
+    path = []
+    current = current_node
+    while current is not None:
+        path.append(current.position)
+        current = current.parent
+    return path[::-1]  # Return reversed path
+
+
+def astar(maze, start, end, allow_diagonal_movement = False):
+    """
+    Returns a list of tuples as a path from the given start to the given end in the given maze
+    :param maze:
+    :param start:
+    :param end:
+    :return:
+    """
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Heapify the open_list and Add the start node
+    heapq.heapify(open_list) 
+    heapq.heappush(open_list, start_node)
+
+    # what squares do we search
+    adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
+    if allow_diagonal_movement:
+        adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1),)
+
+    # Loop until you find the end
+    while len(open_list) > 0:    
+        
+        # Get the current node
+        current_node = heapq.heappop(open_list)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            return return_path(current_node)
+
+        # Generate children
+        children = []
+
+        for new_position in adjacent_squares: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] > chr(ord(maze[current_node.position[0]][current_node.position[1]]) + 1):
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+            # Child is on the closed list
+            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+                continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = abs(child.position[0] - end_node.position[0]) +  abs(child.position[1] - end_node.position[1])
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            if len([open_node for open_node in open_list if child.position == open_node.position and child.g > open_node.g]) > 0:
+                continue
+
+            # Add the child to the open list
+            heapq.heappush(open_list, child)
+
+    warn("Couldn't get a path to destination")
+    return None
+
+
+def compute_part1(map: List[List[str]]) -> int:
+
+    def find_position(letter):
+        for i in range(len(map)):
+            for j in range(len(map[i])):
+                if map[i][j] == letter:
+                    return (i, j)
+    
+    start = find_position('S')
+    end = find_position('E')
+    print(f"Start: {start}, End: {end}")
+    map[start[0]][start[1]] = 'c'
+    map[end[0]][end[1]] = 'z'
+
+    path = astar(map, start, end)
+
+    for step in path:
+        map[step[0]][step[1]] = map[step[0]][step[1]].upper()
+
+    for row in map:
+        print("".join(row))
+
+    print(path)
+    return len(path) - 1
+
+
+def compute_part2(lines: List[str]) -> int:
+    score = 0
+    return score
+
+
+def main(lines: List[str]):
+
+    test_map = [
+        ['S','a','b','q','p','o','n','m'],
+        ['a','b','c','r','y','x','x','l'],
+        ['a','c','c','s','z','E','x','k'],
+        ['a','c','c','t','u','v','w','j'],
+        ['a','b','d','e','f','g','h','i'],
+    ]
+
+    print(f"Test part 1: {compute_part1(test_map)}")
+    map = [list(line) for line in lines]
+    print(f"Part 1: {compute_part1(map)}")
+    print(f"Part 2: {compute_part2(lines)}")
